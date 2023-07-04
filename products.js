0f85e04fb0f600818f327760fbd22930fd7a9ab5
Umme Mohona <ummaymohona488@gmail.com>
2023-07-04 19:28:31 +0600

    product object created

---
+++ products.js
@@ -0,0 +1,41 @@
+const products = [
+    {
+        id: 1,
+        name: 'Women Oversized Pure Cotton T-shirt',
+        image: '1.webp',
+        price: 350
+    },
+    {
+        id: 2,
+        name: 'Women White Twill Parallel Trousers',
+        image: '2.webp',
+        price: 1200
+    },
+    {
+        id: 3,
+        name: 'Women Multicoloured Printed Playsuit',
+        image: '3.avif',
+        price: 2200
+    },
+    {
+        id: 4,
+        name: 'Tasselled Hem Shrug',
+        image: '4.avif',
+        price: 1500
+    },
+    {
+        id: 5,
+        name: 'Women Midi dress',
+        image: '5.jpeg',
+        price: 2700
+    },
+    {
+        id: 6,
+        name: 'Kimono Shrug',
+        image: '6.webp',
+        price: 1000
+    }
+];
+
+
+ export { products};
